315900e34ecf738a3a59ec8b4c7d8719d46b234f
Thomas <thomas@kepow.org>
2017-11-17 01:17:28 +0100

    Add virtual display.

---
+++ astroplant_simulation/actuators.py
@@ -0,0 +1,10 @@
+import asyncio
+from astroplant_kit.peripheral import *
+from . import environment
+
+class VirtualDisplay(Display):
+    def display(self, str):
+        print("---VirtualDisplay---")
+        print(str)
+        print("--------------------")
+
