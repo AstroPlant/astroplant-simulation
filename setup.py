4de0b1fdaecf8e901adb487be4fc03ccd91275cd
Thomas Churchman <thomas@kepow.org>
2020-06-21 20:09:08 +0200

    Improve setup/requirements.

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@
 
 from distutils.core import setup
 
-setup(name='astroplant-simulation',
-      version='0.1',
-      description='AstroPlant environment simulation',
-      author='AstroPlant',
-      author_email='thomas@kepow.org',
-      url='https://astroplant.io',
-      packages=['astroplant_simulation',],
-     )
+setup(
+    name="astroplant-simulation",
+    version="0.1",
+    description="AstroPlant environment simulation",
+    author="AstroPlant",
+    author_email="thomas@kepow.org",
+    url="https://astroplant.io",
+    packages=["astroplant_simulation",],
+    install_requires=["numpy~=1.0", "pillow~=7.0", "schedule~=0.6"],
+)
